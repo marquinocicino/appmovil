cddb2dd65e8714eb3ae877c3ea9821696641120b
Marco Capon <marco1989delrio@gmail.com>
2024-03-16 14:25:51 -0500

    Aprender column row Se aprendio lso sonocicmientos para modificar la primera pantalla

--- app/src/main/java/com/example/myapplicationmae/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationmae/MainActivity.kt
@@ -3,13 +3,21 @@ package com.example.myapplicationmae
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.myapplicationmae.ui.theme.MyApplicationMaeTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +26,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyApplicationMaeTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
+                Surface(  // contenedor elementos
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
@@ -29,18 +37,59 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// modifier mutar decorar alinear  espacios
+// compose crealas intefaces usando componentes funciones
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting(name: String) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Text(
+            text = "Hello $name!",
+            color= Color.Blue,
+            fontSize = 40.sp
+        )
+
+        Text(
+            text = "OTRO TEXTO",
+            color= Color.Green,
+            fontSize = 40.sp
+        )
+    }
+
+}
+
+@Composable
+fun BoxDemo(){
+    Box(
+        modifier = Modifier
+            .size(400.dp)
+    ){
+        Text(
+            text = "Segundo compose",
+            color = Color.Cyan,
+            fontSize = 40.sp,
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+
+        )
+        Text(
+            text = "Texto 2",
+            color = Color.Cyan,
+            fontSize = 40.sp,
+            modifier = Modifier
+                .align(Alignment.CenterStart)
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationMaeTheme {
-        Greeting("Android")
+        Greeting("Android Mae hw a los tiempos")
     }
 }
